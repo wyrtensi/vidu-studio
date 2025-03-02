b31c5179df8c9442aca67e3a8bbb09d4b3e1e78c
wyrtensi <wyrtensi@gmail.com>
2025-03-03 04:35:54 +0500

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-console.log("wyrtensi vidu studio content.js started.");
-chrome.runtime.sendMessage({ action: "injectScript" }, (response) => {
-  if (response && response.success) {
-    console.log("Injected script via background successfully.");
-  } else {
-    console.error("Injection failed: ", response ? response.error : "unknown error");
-  }
-});
